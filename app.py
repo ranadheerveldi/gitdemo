23e26bc825b8e786377610b21f9b65e3acd6c79a
Rakshith <143683496+Rakshithofficial@users.noreply.github.com>
2026-02-02 16:01:14 +0530

    Added greet function using VS Code

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 print("Middle branch")
 print("hello python!!")
+
+
+
+def greet(name):
+    if name.strip() == "":
+        return "Hello, stranger 👋"
+    return f"Hello, {name}! Welcome to Git collaboration."
+
+if __name__ == "__main__":
+    user = input("Enter your name: ")
+    print(greet(user))
+
